2b134f60da7dc44f3ade9457dbe8b9647adc390f
rhit-allenjj <allenjj@rose-hulman.edu>
2024-04-03 22:01:11 -0400

    made classes

---
+++ Jaxon/homePage.java
@@ -0,0 +1,16 @@
+package Jaxon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class homePage {
+   
+   public void displayWindow() {
+    JFrame frame = new JFrame();
+		frame.setBounds(100, 100, 300, 130);
+        JPanel panel = new JPanel();
+   }
+   public static void main(String[] args) {
+    homePage.displayWindow();
+}
+}
