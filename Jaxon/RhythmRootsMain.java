57007b4a7fd7ac64ad3fb77d092c8ba8f641847a
rhit-allenjj <allenjj@rose-hulman.edu>
2024-04-04 09:44:15 -0400

    nothing

--- Jaxon/RhythmRootsMain.java
+++ Jaxon/RhythmRootsMain.java
@@ -1,5 +1,20 @@
 package Jaxon;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 public class RhythmRootsMain {
     homePage.displayWindow();
+}
+
+
+
+public class homePage {
+   
+   public static void displayWindow() {
+    JFrame frame = new JFrame();
+		frame.setBounds(100, 100, 300, 130);
+        JPanel panel = new JPanel();
+   }
+   public static void main(String[] args) {
+    homePage.displayWindow();
+}
 }
