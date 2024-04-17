ebd8fa72d5fe72c590cb3f687ccf69c0fad00b21
rhit-bernardj <119466548+rhit-bernardj@users.noreply.github.com>
2024-04-17 15:15:31 -0400

    fixing

--- Results.java
+++ Results.java
@@ -3,24 +3,29 @@ import java.util.List;
 import java.util.Map;
 
 public class Results {
-    private Questionaire questionaireItem;
+    private Questionaire questionnaire;
 
-    public Results(Questionaire questionnaireItem) {
-        this.questionaireItem = questionnaireItem;
+    public Results(Questionaire questionnaire) {
+        this.questionnaire = questionnaire;
     }
 
     // Method to calculate the top artists based on selected answers
     public Map<String, Integer> calculateTopArtists() {
         Map<String, Integer> artistPoints = new HashMap<>();
-        List<Answer> selectedAnswers = questionaireItem.getSelectedAnswers();
-        
+        List<Answer> selectedAnswers = questionnaire.getSelectedAnswers();
+
+        // Sample artists data
+        Artists artists = new Artists();
+
         // Calculate points for each artist based on selected answers
         for (Answer answer : selectedAnswers) {
             String artist = answer.getArtist();
             int points = answer.getPoints();
-            artistPoints.put(artist, artistPoints.getOrDefault(artist, 0) + points);
+            if (artist != null && !artist.isEmpty()) {
+                artistPoints.put(artist, artistPoints.getOrDefault(artist, 0) + points);
+            }
         }
-        
+
         return artistPoints;
     }
 
@@ -28,14 +33,14 @@ public class Results {
     public String generateTopArtistsHTML() {
         Map<String, Integer> topArtists = calculateTopArtists();
         StringBuilder html = new StringBuilder();
-        
+
         // Add HTML for each top artist
         for (Map.Entry<String, Integer> entry : topArtists.entrySet()) {
             String artist = entry.getKey();
             int points = entry.getValue();
             html.append("<div>").append(artist).append(": ").append(points).append(" points</div>");
         }
-        
+
         return html.toString();
     }
 }
