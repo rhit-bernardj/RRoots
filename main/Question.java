66044be2f114a3c0b27375d683c80805e476efe6
rhit-allenjj <allenjj@rose-hulman.edu>
2024-04-26 10:09:41 -0400

    updated main package

---
+++ main/Question.java
@@ -0,0 +1,24 @@
+package main;
+
+
+import java.util.ArrayList;
+
+// public abstract class Question {
+     
+//      String question = new String();
+//      ArrayList<String> answerOptions = new ArrayList<>();  
+
+//      public Question(String question, ArrayList<String> answerOptions) {
+//           this.question = question;
+//           this.answerOptions = answerOptions;
+//       }
+
+//       public ArrayList<String> getAnswerOptions() {
+//           return answerOptions;
+//       } 
+
+// }
+
+
+
+
